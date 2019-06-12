430b1dda10beb0bb50c438829f026bfc6c316b3f
gusvagner <gustavovagner@hotmail.com>
2019-06-12 18:31:34 -0300

    entry service

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError, flatMap } from 'rxjs/operators';
 import { Entry } from './entry.model';
+import { CategoryService } from '../../categories/shared/category.service'
 import { element } from '@angular/core/src/render3';
 
 @Injectable({
@@ -12,7 +13,7 @@ export class EntryService {
 
   private apiPath: string = "api/entries";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private categoryService: CategoryService) { }
 
   getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(
@@ -21,8 +22,8 @@ export class EntryService {
     )
   }
 
-  getById(id : number): Observable<Entry> {
-    const url =  `${this.apiPath}/${id}`;    
+  getById(id: number): Observable<Entry> {
+    const url = `${this.apiPath}/${id}`;
     return this.http.get(url).pipe(
       catchError(this.handleError),
       map(this.jsonDataToEntry)
@@ -30,22 +31,39 @@ export class EntryService {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToEntry)
+        )
+      })
     )
+
+
   }
 
   update(entry: Entry): Observable<Entry> {
-    const url =  `${this.apiPath}/${entry.id}`; 
-    return this.http.put(url, entry).pipe(
-      catchError(this.handleError),
-      map(() => entry) // only in in-memory, because there's nothing to reurn
+    const url = `${this.apiPath}/${entry.id}`;
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category
+
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry) // only in in-memory, because there's nothing to reurn
+        )
+      })
     )
+
   }
-  
+
   delete(id: number): Observable<any> {
-    const url = `${this.apiPath}/${id}`; 
+    const url = `${this.apiPath}/${id}`;
     return this.http.delete(url).pipe(
       catchError(this.handleError),
       map(() => null)
@@ -59,7 +77,7 @@ export class EntryService {
 
     jsonData.forEach(element => {
       const entry = Object.assign(new Entry(), element);
-      entries.push(entry);      
+      entries.push(entry);
     });
     return entries;
   }
